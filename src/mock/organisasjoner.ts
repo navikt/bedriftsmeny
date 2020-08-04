c04f44c412ccf4fe23b4078a92f4a5c0aa8b147c
Terese Haug <terese.haug@nav.no>
2020-08-04 10:45:13 +0200

    Tester med flere underenheter

--- src/mock/organisasjoner.ts
+++ src/mock/organisasjoner.ts
@@ -1,181 +1,317 @@
 import { Organisasjon} from '../bedriftsmeny/organisasjon';
 
 export const MOCK_ORGANISASJONER: Organisasjon[] = [
-    {
-        Name: 'BALLSTAD OG EIDSLANDET',
-        Type: 'Business',
-        OrganizationNumber: '811076422',
-        ParentOrganizationNumber: '811076112',
-        OrganizationForm: 'AAFY',
-        Status: 'Active'
-    },
-    {
-        Name: 'BALLSTAD OG HAMARØY',
-        Type: 'Business',
-        OrganizationNumber: '811076732',
-        ParentOrganizationNumber: '811076112',
-        OrganizationForm: 'BEDR',
-        Status: 'Active'
-    },
-    {
-        Name: 'BALLSTAD OG HORTEN',
-        Type: 'Enterprise',
-        OrganizationNumber: '811076112',
-        ParentOrganizationNumber: '',
-        OrganizationForm: 'FLI',
-        Status: 'Active'
-    },
-    {
-        Name: 'BALLSTAD OG SÆTERVIK',
-        Type: 'Business',
-        OrganizationNumber: '811076902',
-        ParentOrganizationNumber: '811076112',
-        OrganizationForm: 'BEDR',
-        Status: 'Active'
-    },
-    {
-        Name: 'BAREKSTAD OG YTTERVÅG REGNSKAP',
-        Type: 'Enterprise',
-        ParentOrganizationNumber: '',
-        OrganizationNumber: '810514442',
-        OrganizationForm: 'AS',
-        Status: 'Active'
-    },
-    {
-        Name: 'BIRI OG VANNAREID REVISJON',
-        Type: 'Enterprise',
-        ParentOrganizationNumber: '',
-        OrganizationNumber: '910998250',
-        OrganizationForm: 'AS',
-        Status: 'Active'
-    },
-    {
-        Name: 'EIDSNES OG AUSTRE ÅMØY',
-        Type: 'Business',
-        OrganizationNumber: '910521551',
-        ParentOrganizationNumber: '910998250',
-        OrganizationForm: 'BEDR',
-        Status: 'Active'
-    },
-    {
-        Name: 'FRØNNINGEN OG LAUVSTAD REVISJON',
-        Type: 'Enterprise',
-        ParentOrganizationNumber: '',
-        OrganizationNumber: '910223208',
-        OrganizationForm: 'AS',
-        Status: 'Active'
-    },
-    {
-        Name: 'HARSTAD OG TYSSEDAL REVISJON',
-        Type: 'Enterprise',
-        ParentOrganizationNumber: '',
-        OrganizationNumber: '810989572',
-        OrganizationForm: 'AS',
-        Status: 'Active'
-    },
-    {
-        Name: 'HAVNNES OG ÅGSKARDET',
-        Type: 'Enterprise',
-        ParentOrganizationNumber: '',
-        OrganizationNumber: '910646176',
-        OrganizationForm: 'AS',
-        Status: 'Active'
-    },
-    {
-        Name: 'KJØLLEFJORD OG ØKSFJORD',
-        Type: 'Enterprise',
-        ParentOrganizationNumber: '',
-        OrganizationNumber: '910175777',
-        OrganizationForm: 'AS',
-        Status: 'Active'
-    },
-    {
-        Name: 'KYSTBASEN ÅGOTNES OG ILSENG REGNSKAP',
-        Type: 'Enterprise',
-        OrganizationNumber: '910514318',
-        ParentOrganizationNumber: '910175777',
-        OrganizationForm: 'ASA',
-        Status: 'Active'
-    },
-    {
-        Name: 'SANDVÆR OG HOV',
-        Type: 'Business',
-        OrganizationNumber: '910793829',
-        OrganizationForm: 'BEDR',
-        ParentOrganizationNumber: '910720120',
-        Status: 'Active'
-    },
-    {
-        Name: 'SKOTSELV OG HJELSET',
-        Type: 'Enterprise',
-        ParentOrganizationNumber: '',
-        OrganizationNumber: '910720120',
-        OrganizationForm: 'AS',
-        Status: 'Active'
-    },
-    {
-        Name: 'STOL PÅ TORE',
-        Type: 'Enterprise',
-        ParentOrganizationNumber: '',
-        OrganizationNumber: '810771852',
-        OrganizationForm: 'AS',
-        Status: 'Active'
-    },
-    {
-        Name: 'SØR-HIDLE OG STRAUMGJERDE',
-        Type: 'Enterprise',
-        ParentOrganizationNumber: '',
-        OrganizationNumber: '910167200',
-        OrganizationForm: 'AS',
-        Status: 'Active'
-    },
-    {
-        Name: 'TROMVIK OG SPARBU REVISJON',
-        Type: 'Business',
-        OrganizationNumber: '910989626',
-        ParentOrganizationNumber: '910167200',
-        OrganizationForm: 'BEDR',
-        Status: 'Active'
-    },
-    {
-        Name: 'Tore sitt testselskap',
-        Type: 'Enterprise',
-        ParentOrganizationNumber: '',
-        OrganizationNumber: '910820834',
-        OrganizationForm: 'AS',
-        Status: 'Active'
-    },
-    {
-        Name: 'UGGDAL OG STEINSDALEN',
-        Type: 'Business',
-        OrganizationNumber: '910521616',
-        ParentOrganizationNumber: '910820834',
-        OrganizationForm: 'BEDR',
-        Status: 'Active'
-    },
-    {
-        Name: 'VALESTRANDSFOSSEN OG SØRLI REVISJON',
-        Type: 'Business',
-        OrganizationNumber: '810989602',
-        ParentOrganizationNumber: '910820834',
-        OrganizationForm: 'BEDR',
-        Status: 'Active'
-    },
-    {
-        Name: 'VESTBY OG LOEN REVISJON',
-        Type: 'Business',
-        OrganizationNumber: '910989642',
-        ParentOrganizationNumber: '910820834',
-        OrganizationForm: 'BEDR',
-        Status: 'Active'
-    },
-    {
-        Name: 'DIGITAL JUNKIES AS ',
-        Type: 'Business',
-        OrganizationNumber: '922658986',
-        ParentOrganizationNumber: '822565212',
-        OrganizationForm: 'BEDR',
-        Status: 'Active'
-    }
+        {
+            Name: 'BALLSTAD OG EIDSLANDET',
+            Type: 'Business',
+            OrganizationNumber: '811076422',
+            ParentOrganizationNumber: '811076112',
+            OrganizationForm: 'AAFY',
+            Status: 'Active'
+        },
+        {
+            Name: 'BALLSTAD OG HAMARØY',
+            Type: 'Business',
+            OrganizationNumber: '811076732',
+            ParentOrganizationNumber: '811076112',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'BALLSTAD OG HORTEN',
+            Type: 'Enterprise',
+            OrganizationNumber: '811076112',
+            ParentOrganizationNumber: '',
+            OrganizationForm: 'FLI',
+            Status: 'Active'
+        },
+        {
+            Name: 'BALLSTAD OG SÆTERVIK',
+            Type: 'Business',
+            OrganizationNumber: '811076902',
+            ParentOrganizationNumber: '811076112',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'BAREKSTAD OG YTTERVÅG REGNSKAP',
+            Type: 'Enterprise',
+            ParentOrganizationNumber: '',
+            OrganizationNumber: '810514442',
+            OrganizationForm: 'AS',
+            Status: 'Active'
+        },
+        {
+            Name: 'BIRI OG VANNAREID REVISJON',
+            Type: 'Enterprise',
+            ParentOrganizationNumber: '',
+            OrganizationNumber: '910998250',
+            OrganizationForm: 'AS',
+            Status: 'Active'
+        },
+        {
+            Name: 'EIDSNES OG AUSTRE ÅMØY',
+            Type: 'Business',
+            OrganizationNumber: '910521551',
+            ParentOrganizationNumber: '910998250',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'FRØNNINGEN OG LAUVSTAD REVISJON',
+            Type: 'Enterprise',
+            ParentOrganizationNumber: '',
+            OrganizationNumber: '910223208',
+            OrganizationForm: 'AS',
+            Status: 'Active'
+        },
+        {
+            Name: 'HARSTAD OG TYSSEDAL REVISJON',
+            Type: 'Enterprise',
+            ParentOrganizationNumber: '',
+            OrganizationNumber: '810989572',
+            OrganizationForm: 'AS',
+            Status: 'Active'
+        },
+        {
+            Name: 'HAVNNES OG ÅGSKARDET',
+            Type: 'Enterprise',
+            ParentOrganizationNumber: '',
+            OrganizationNumber: '910646176',
+            OrganizationForm: 'AS',
+            Status: 'Active'
+        },
+        {
+            Name: 'KJØLLEFJORD OG ØKSFJORD',
+            Type: 'Enterprise',
+            ParentOrganizationNumber: '',
+            OrganizationNumber: '910175777',
+            OrganizationForm: 'AS',
+            Status: 'Active'
+        },
+        {
+            Name: 'KYSTBASEN ÅGOTNES OG ILSENG REGNSKAP',
+            Type: 'Enterprise',
+            OrganizationNumber: '910514318',
+            ParentOrganizationNumber: '910175777',
+            OrganizationForm: 'ASA',
+            Status: 'Active'
+        },
+        {
+            Name: 'SANDVÆR OG HOV',
+            Type: 'Business',
+            OrganizationNumber: '910793829',
+            OrganizationForm: 'BEDR',
+            ParentOrganizationNumber: '910720120',
+            Status: 'Active'
+        },
+        {
+            Name: 'SKOTSELV OG HJELSET',
+            Type: 'Enterprise',
+            ParentOrganizationNumber: '',
+            OrganizationNumber: '910720120',
+            OrganizationForm: 'AS',
+            Status: 'Active'
+        },
+        {
+            Name: 'STOL PÅ TORE',
+            Type: 'Enterprise',
+            ParentOrganizationNumber: '',
+            OrganizationNumber: '810771852',
+            OrganizationForm: 'AS',
+            Status: 'Active'
+        },
+        {
+            Name: 'SØR-HIDLE OG STRAUMGJERDE VELDIG LANGT OG ENDA LENGRE',
+            Type: 'Enterprise',
+            ParentOrganizationNumber: '',
+            OrganizationNumber: '910167200',
+            OrganizationForm: 'AS',
+            Status: 'Active'
+        },
+        {
+            Name: 'TROMVIK OG SPARBU REVISJON',
+            Type: 'Business',
+            OrganizationNumber: '910989626',
+            ParentOrganizationNumber: '910167200',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'Tore sitt testselskap',
+            Type: 'Enterprise',
+            ParentOrganizationNumber: '',
+            OrganizationNumber: '910820834',
+            OrganizationForm: 'AS',
+            Status: 'Active'
+        },
+        {
+            Name: 'UGGDAL OG STEINSDALEN',
+            Type: 'Business',
+            OrganizationNumber: '910521616',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VALESTRANDSFOSSEN OG SØRLI OG SØNN REVISJON',
+            Type: 'Business',
+            OrganizationNumber: '810989602',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VESTBY OG LOEN OG LAAANGT ETTERNAVN REVISJON',
+            Type: 'Business',
+            OrganizationNumber: '910989642',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VESTBY OG LOEN 1',
+            Type: 'Business',
+            OrganizationNumber: '910989643',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VESTBY OG LOEN 2',
+            Type: 'Business',
+            OrganizationNumber: '910989644',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VESTBY OG LOEN 3',
+            Type: 'Business',
+            OrganizationNumber: '910989645',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VESTBY OG LOEN 4',
+            Type: 'Business',
+            OrganizationNumber: '910989646',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VESTBY OG LOEN 5',
+            Type: 'Business',
+            OrganizationNumber: '910989647',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VESTBY OG LOEN 6',
+            Type: 'Business',
+            OrganizationNumber: '910989648',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VESTBY OG LOEN 7',
+            Type: 'Business',
+            OrganizationNumber: '910989649',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VESTBY OG LOEN 8',
+            Type: 'Business',
+            OrganizationNumber: '910989650',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VESTBY OG LOEN 9',
+            Type: 'Business',
+            OrganizationNumber: '910989651',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VESTBY OG LOEN 10',
+            Type: 'Business',
+            OrganizationNumber: '910989652',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VESTBY OG LOEN 11',
+            Type: 'Business',
+            OrganizationNumber: '910989653',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VESTBY OG LOEN 12',
+            Type: 'Business',
+            OrganizationNumber: '910989654',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VESTBY OG LOEN 13',
+            Type: 'Business',
+            OrganizationNumber: '910989655',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VESTBY OG LOEN 14',
+            Type: 'Business',
+            OrganizationNumber: '910989656',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VESTBY OG LOEN 15',
+            Type: 'Business',
+            OrganizationNumber: '910989657',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VESTBY OG LOEN 16',
+            Type: 'Business',
+            OrganizationNumber: '910989658',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'VESTBY OG LOEN 17',
+            Type: 'Business',
+            OrganizationNumber: '910989659',
+            ParentOrganizationNumber: '910820834',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        },
+        {
+            Name: 'DIGITAL JUNKIES AS ',
+            Type: 'Business',
+            OrganizationNumber: '922658986',
+            ParentOrganizationNumber: '822565212',
+            OrganizationForm: 'BEDR',
+            Status: 'Active'
+        }
     ]
 ;
