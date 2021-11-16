3524d29d06afb8e7b284d8485c6ac07edbd920ea
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2021-11-16 09:00:26 +0100

    bruk unik key til local store

--- src/bedriftsmeny/Virksomhetsvelger/utils/utils.ts
+++ src/bedriftsmeny/Virksomhetsvelger/utils/utils.ts
@@ -3,6 +3,7 @@ import { History } from 'history';
 import { Organisasjon, JuridiskEnhetMedUnderEnheterArray } from '../../organisasjon';
 
 const ORGNUMMER_PARAMETER = 'bedrift';
+const ORGNUMMER_LOCAL_STORE = 'virksomhetsvelger_bedrift';
 
 export const settOrgnummerIUrl = (orgnummer: string, history: History) => {
     const currentUrl = new URL(window.location.href);
@@ -13,10 +14,10 @@ export const settOrgnummerIUrl = (orgnummer: string, history: History) => {
 };
 
 export const getLocalStorageOrgnr = (): string | null =>
-    window.localStorage.getItem(ORGNUMMER_PARAMETER);
+    window.localStorage.getItem(ORGNUMMER_LOCAL_STORE);
 
 export const setLocalStorageOrgnr = (orgnr: string): void =>
-    window.localStorage.setItem(ORGNUMMER_PARAMETER, orgnr);
+    window.localStorage.setItem(ORGNUMMER_LOCAL_STORE, orgnr);
 
 export const hentUnderenheter = (organisasjonstre: JuridiskEnhetMedUnderEnheterArray[]) =>
     organisasjonstre.reduce(
