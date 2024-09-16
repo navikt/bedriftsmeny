899afe430180968dfa66714a16def09faf36ed14
Ken Gullaksen <ken.gullaksen@nav.no>
2024-09-16 11:47:21 +0200

    fjern type og status fra Organisasjon interface

--- src/bedriftsmeny/organisasjon.ts
+++ src/bedriftsmeny/organisasjon.ts
@@ -1,9 +1,7 @@
 export interface Organisasjon {
     Name: string;
-    Type: string;
     OrganizationNumber: string;
     OrganizationForm: string;
-    Status: string;
     ParentOrganizationNumber: string;
 }
 
