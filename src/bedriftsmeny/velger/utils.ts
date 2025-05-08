dcf9ae7fb8592617773349388fb5f5a2d2eec790
Jonas Enge <jonas.maccyber.enge@nav.no>
2025-05-08 15:06:37 +0200

    Endrer tilbake fra react-router til react-router-dom

--- src/bedriftsmeny/velger/utils.ts
+++ src/bedriftsmeny/velger/utils.ts
@@ -1,5 +1,5 @@
 import { Organisasjon, JuridiskEnhetMedUnderEnheterArray } from '../organisasjon';
-import { useLocation, useNavigate } from 'react-router';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const ORGNUMMER_PARAMETER = 'bedrift';
 const ORGNUMMER_LOCAL_STORE = 'virksomhetsvelger_bedrift';
